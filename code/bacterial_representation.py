ee538572b9b5d368bc17ec9a8d2d9e951c76e07e
Lucy Androsiuk <lucyandrosyuk@gmail.com>
2022-11-30 15:33:46 +0200

    Changes to bacterial_representation.py for cluster

--- code/bacterial_representation.py
+++ code/bacterial_representation.py
@@ -22,21 +22,22 @@ pd.set_option('display.max_columns', None)
 email = "androsiu@post.bgu.ac.il"  # Tell NCBI who you are
 # uncomment relevant path to OS
 # Windows
-path = r"C:\Users\Lucy\iCloudDrive\Documents\bengurion\Project students\Sivan_project"
+#path = r"C:\Users\Lucy\iCloudDrive\Documents\bengurion\Project students\Sivan_project"
 # macOS
 #path = r"/Users/lucyandrosiuk/Documents/bengurion/Project students/Sivan_project"
 # Cluster
-#path = r"/gpfs0/tals/projects/Analysis/Lucy_plasmidome/Plasmidome/CRISPR"
+path = r"/gpfs0/tals/projects/Analysis/Lucy_plasmidome/Plasmidome/CRISPR"
 
 # working directories
 visuals = f"{path}/visualisations"
 tables = f"{path}/data_calculations"
+resource = f"{path}/res"
 Path(visuals).mkdir(parents=True, exist_ok=True)
 
 # working files
-blast_results = r"../res/BLASTp_resultsRatio.zip"
-plsdb_meta = r"../res/plsdb.zip"
-spacers_meta = r"../res/spacer_seqName.fsa"
+blast_results = f"{resource}/BLASTp_resultsRatio.zip"
+plsdb_meta =f"{resource}/plsdb.zip"
+spacers_meta = f"{resource}/spacer_seqName.fsa"
 
 # managing metadata for spacers db
 def search_Entrez (x):
@@ -69,7 +70,7 @@ def DF_from_fasta():
     with open (taxon_csv, 'w') as csv_file:
         csv_file.write("ID,Taxonomy,Species\n")
         print("There are %d records" % len(unique_id))
-        for id in unique_id[:3]:
+        for id in unique_id:
             ind = np.where(unique_id == id) # retrieving id's index
             print("The index is %d" % ind)
             sp, tax = search_Entrez(id) # retrieving species and taxonomy info for each id
@@ -87,15 +88,16 @@ def DF_from_fasta():
     df_final['Kingdom'] = df_final['Taxonomy'].apply(lambda x: x[0])
     df_final.drop('Taxonomy', axis=1, inplace = True)
     print(df_final)
-    final_csv = f'{tables}/.csv'
-    #df_final.to_csv(final_csv, index = False)
+    final_csv = f'{tables}/spacers_taxon.csv'
+    if not os.path.isfile(final_csv) or os.stat(final_csv).st_size == 0:
+        df_final.to_csv(final_csv, index = False)
     return df_final
 #DF_from_fasta()
 
 def plsdb():
     # metadata for plasmid db
     plsdb_hosts = pd.read_csv(plsdb_meta, delimiter='\t', header=0,  error_bad_lines=False ,usecols=[1,28 ,30,32 , 34, 36, 38, 40])
-    print(plsdb_hosts)
+    #print(plsdb_hosts)
     return plsdb_hosts
 
 
@@ -104,10 +106,10 @@ def blast_res():
     plasmid_blast = pd.read_csv(blast_results, sep= ",",  header = 0)
     df_spacers = DF_from_fasta()
     df_plasmids = plsdb()
-    print(plasmid_blast)
+    #print(plasmid_blast)
     blast_df = pd.merge(plasmid_blast, df_plasmids, left_on = 'qseqid' , right_on =  'ACC_NUCCORE', how = 'left')
     blast_df = pd.merge(blast_df, df_spacers, left_on = 'stitle' , right_on = 'ID', how = 'left')
-    print(blast_df)
+    #print(blast_df)
     return blast_df
 df_blast = blast_res()
 
@@ -143,21 +145,21 @@ def prob_func(x, level, popul, df_popul, df_blast):
     print(N, A, n, k)
     rv = hypergeom(N, A, n)
     pmf_level = rv.pmf(k)
-    print(pmf_level)
+    #print(pmf_level)
     pval = hypergeom.sf(k-1, N, A, n)
-    print(pval)
+    #print(pval)
 
     level_freq = (A/N)*100
     level_blast_freq = (k/n)*100
     #print (pmf_cog)
-    print('The probability of getting %d %s with host %s at %s level out of %d hits is %s.' % (k, popul, x, level.capitalize(), n, "{:.2e}".format(pmf_level)))
-    print('The probability of getting %d or more %s with host %s at %s level out of %d hits is %s.' % (k, popul, x, level.capitalize(), n, "{:.2e}".format(pval)))
+    #print('The probability of getting %d %s with host %s at %s level out of %d hits is %s.' % (k, popul, x, level.capitalize(), n, "{:.2e}".format(pmf_level)))
+    #print('The probability of getting %d or more %s with host %s at %s level out of %d hits is %s.' % (k, popul, x, level.capitalize(), n, "{:.2e}".format(pval)))
     print('The frequency of %s with host %s at %s level in %s-database: (%d/%d)*100=%f' % (popul, x, level.capitalize(), popul, A, N, round(level_freq,2)))
     print('The frequency of %s with host %s at %s in our blast results: (%d/%d)*100=%f' % (popul, x, level.capitalize(), k, n, round(level_blast_freq, 2)))
     #not working
-    prob = hypergeom.pmf(N, A, n, k)
-    print(prob)
-    return pmf_level
+    #prob = hypergeom.pmf(N, A, n, k)
+    #print(prob)
+    return level_freq, level_blast_freq
 
 def getting_levels(subj):
     if subj == 'plasmids':
@@ -184,4 +186,5 @@ def getting_probability(subj, df_popul):
             prob_func(taxa, level, subj, df_popul, df_blast)
 
 getting_probability('plasmids', plsdb())
+getting_probability('spacers', plsdb())
 
