087b35b5c8d495e2a75a1ff2c10b88bbd2b174d8
lucy-androsiuk <lucyandrosyuk@gmail.com>
2023-01-18 18:38:22 +0200

    Update metadata.py
    removed unnecessary variables, change direcories

--- code/metadata.py
+++ code/metadata.py
@@ -15,8 +15,8 @@ import sys
 import getopt
 
 
-#Read BLASTn
-plasmid_blast = pd.read_csv ("C:/sivan/לימודים/שנה ג/פרויקט מחקר בפייתון/BLASTp_resultsRatio.csv", sep= ",",  header = 0)
+#Read BLASTn file
+plasmid_blast = pd.read_csv ("/path/to/Blast_ratio.csv", sep= ",",  header = 0)
 plasmid_blast_copy = plasmid_blast.copy()
 
 
@@ -26,21 +26,15 @@ plasmid_blast_copy2 = plasmid_blast_copy2.drop_duplicates()
 
 
 #Read PLSDB file - chosen columns with taxonomy
-with open("/gpfs0/tals/projects/Analysis/sivan_project/plsdb.tsv") as f:
+with open("/path/to/the/plsdb/metadata/plsdb.tsv") as f:
     features_train = pd.read_csv(f, delimiter='\t', header=None,  error_bad_lines=False ,usecols=[1,28 ,30,32 , 34, 36, 38, 40])
     features_train_copy = features_train.copy()
 
-
-#Read BLASTn file    
-main_file = pd.read_csv ("/gpfs0/tals/projects/Analysis/sivan_project/BLASTp_resultsRatio.csv", sep= ",",  header = 0)
-main_file_copy = main_file.copy()   
-    
 #Merge by id the files in order to get a file with ids from the BLASTn results and taxonomy from PLSDB metadata    
 plasmid_blast_copy2 = plasmid_blast_copy2.merge(features_train_copy, left_on= "0", right_on=1)
 
 #Merge by id the files in order to get a file of BLASTn results and the taxonomy of thr plasmids 
-main_file_copy = main_file_copy.merge(plasmid_blast_copy2, left_on= "qseqid", right_on="0")
+plasmid_blast_copy = plasmid_blast_copy.merge(plasmid_blast_copy2, left_on= "qseqid", right_on="0")
 
-
-plasmid_blast_copy.to_csv("unique_taxonomy_metadata2.csv")
+plasmid_blast_copy2.to_csv("unique_taxonomy_metadata.csv")
 
