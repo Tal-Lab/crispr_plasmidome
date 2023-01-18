cc5e948fe25225812c9e2cd988ac2a600f4bd81f
lucy-androsiuk <lucyandrosyuk@gmail.com>
2023-01-18 17:42:16 +0200

    Update Research_project.py
    Replaced paths to files with system arguments for extended usage.

--- code/Research_project.py
+++ code/Research_project.py
@@ -10,14 +10,14 @@ import os
 
 #Turn spacer fasta file in to data base
 from Bio.Blast.Applications import NcbimakeblastdbCommandline
-makedb = NcbimakeblastdbCommandline(cmd='makeblastdb', dbtype='nucl', input_file='/gpfs0/tals/projects/Analysis/sivan_project/spacers_db/20210121_spacer_34.fasta', out = "spacersdb")
+makedb = NcbimakeblastdbCommandline(cmd='makeblastdb', dbtype='nucl', input_file=sys.argv[1], out = "spacersdb")
 os.system(str(makedb))
 stdout, stderr = makedb()
 
 #Preform BLASTn on PLSDB
 #This process was done on marine plasmids as well
 from Bio.Blast.Applications import NcbiblastnCommandline
-makeblast = NcbiblastnCommandline(query="/gpfs0/tals/projects/Analysis/sivan_project/plasmid_db/plsdb.fna", db='/gpfs0/tals/projects/Analysis/sivan_project/spacersdb' ,evalue=0.00001, perc_identity= 90, ungapped=True, outfmt = "6 qseqid stitle sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore", out = "Blast.csv")
+makeblast = NcbiblastnCommandline(query=sys.argv[2], db='/path/to/your/database/direcctory/spacersdb' ,evalue=0.00001, perc_identity= 90, ungapped=True, outfmt = "6 qseqid stitle sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore", out = "Blast.csv")
 os.system(str(makeblast))
 stdout, stderr = makeblast()
 
